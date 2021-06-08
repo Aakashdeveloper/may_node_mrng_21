3cdd3f8e0012366f7b6fc7ef086eb0165c1bb361
aakashdeveloper <ahanda205@gmail.com>
2021-06-08 06:55:52 +0530

    restapi

--- dashboard/apiWithOutUi.js
+++ dashboard/apiWithOutUi.js
@@ -10,7 +10,8 @@ let db;
 let col_name = "dashboard"
 
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/health',(req,res)=>{
     res.status(200).send('Health Check')
@@ -18,8 +19,101 @@ app.get('/health',(req,res)=>{
 
 
 //Read
+app.get('/users',(req,res) => {
+    var query = {}
+    if(req.query.role && req.query.city){
+        query={role:req.query.role,city:req.query.city,isActive:true}
+    }
+    else if(req.query.city){
+        query={city:req.query.city,isActive:true}
+    }
+    else{
+        query = {isActive:true}
+    }
+    db.collection(col_name).find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
 
 
+//userDetails
+app.get('/user/:id',(req,res) => {
+    var id = mongo.ObjectID(req.params.id)
+    db.collection(col_name).find({_id:id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+//Insert
+app.post('/addUser',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name).insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('Data Added')
+    })
+})
+
+//update
+app.put('/updateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectID(req.body._id)},
+        {
+            $set:{
+                name: req.body.name,
+                city: req.body.city,
+                phone: req.body.phone,
+                role: req.body.role,
+                isActive: true
+            }
+        },(err,result) =>{
+            if(err) throw err;
+            res.send('Data Updated')
+        }
+    )
+})
+
+
+//delete
+app.delete('/deleteUser',(req,res)=> {
+    db.collection(col_name).remove({_id:mongo.ObjectID(req.body._id)},(err,result)=>{
+        if(err) throw err;
+        res.send('Data Deleted')
+    })
+})
+
+//softDelete(deactivate)
+app.put('/deactivateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectID(req.body._id)},
+        {
+            $set:{
+                isActive: false
+            }
+        },(err,result) =>{
+            if(err) throw err;
+            res.send('User Deactivated')
+        }
+    )
+})
+
+
+//activate
+app.put('/activateUser',(req,res) => {
+    db.collection(col_name).updateOne(
+        {_id:mongo.ObjectID(req.body._id)},
+        {
+            $set:{
+                isActive: true
+            }
+        },(err,result) =>{
+            if(err) throw err;
+            res.send('User Activated')
+        }
+    )
+})
 
 
 //Db Connection
