2f3d35bd1fa0504da1384d7e986b4f4dffc62d80
aakashdeveloper <ahanda205@gmail.com>
2021-06-15 06:57:49 +0530

    oonode

---
+++ oonode_js/database.js
@@ -0,0 +1,39 @@
+import mongodb from 'mongodb';
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017";
+const dbName = "junenode";
+
+const MainCall = {}
+
+var output;
+MainCall.getData=(colName)=>{
+    MongoClient.connect(url,(err,conn)=>{
+        if(err) throw err;
+        var dbo = conn.db(dbName);
+        dbo.collection(colName).find({}).toArray((err,result) => {
+            if(err) throw err;
+            console.log('Data fetched')
+            output = result
+        })
+    })
+
+    return output
+}
+
+MainCall.postData=(colName,dbObj)=>{
+    MongoClient.connect(url,(err,conn)=>{
+        if(err) throw err;
+        var dbo = conn.db(dbName);
+        dbo.collection(colName).insertOne(dbObj,(err,result) => {
+            if(err) throw err;
+            console.log('Data Added')
+        })
+    })
+
+    return 'Data Added'
+}
+
+
+
+
+export default MainCall
