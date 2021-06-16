e4ec572552bb716116d261ecdf70692d1edb280e
aakashdeveloper <ahanda205@gmail.com>
2021-06-16 06:54:12 +0530

    graphql

---
+++ graphqlApi/Schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const{
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLFloat,
+    GraphQLSchema
+}=graphql
+
+const MovieType = new GraphQLObjectType({
+    name:'Movies',
+    fields:{
+        id: {type:GraphQLInt},
+        name: {type:GraphQLString},
+        language: {type:GraphQLString},
+        rate: {type:GraphQLFloat},
+        type: {type:GraphQLString},
+        imageUrl: {type:GraphQLString}
+    }
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        Movies:{
+            type:MovieType,
+            args:{id:{type:GraphQLInt}},
+            resolve(parentValue,args){
+                return axios.get(`http://localhost:9800/movies/${args.id}`)
+                .then((res) => res.data)
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
