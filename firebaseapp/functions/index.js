6beda693d633ee4e1cfbdb0c8fa91a0a31f476f9
aakashdeveloper <ahanda205@gmail.com>
2021-06-17 07:06:55 +0530

    final commit

---
+++ firebaseapp/functions/index.js
@@ -0,0 +1,48 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser')
+const firebase = require('firebase')
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const db = admin.firestore();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+
+    const citiesRef = db.collection('nodecities');
+
+    await citiesRef.doc('LA').set({
+        name: 'Los Angeles', state: 'CA', country: 'USA',
+        capital: false, population: 3900000
+      });
+
+      await citiesRef.doc('DC').set({
+        name: 'Washington, D.C.', state: null, country: 'USA',
+        capital: true, population: 680000
+      });
+      await citiesRef.doc('TOK').set({
+        name: 'Tokyo', state: null, country: 'Japan',
+        capital: true, population: 9000000
+      });
+      await citiesRef.doc('BJ').set({
+        name: 'Beijing', state: null, country: 'China',
+        capital: true, population: 21500000
+      });
+
+});
+
+
+exports.getMessage = functions.https.onRequest(async (req, res) => {
+    const citiesRef = db.collection('nodecities');
+    const snapshot = await citiesRef.get();
+    var out = []
+    snapshot.forEach(doc => {
+        out.push(doc.data())
+    });
+    res.send(out);
+});
